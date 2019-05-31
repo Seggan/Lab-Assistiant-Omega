8e57a936392508c9c1b712eb598aeb3d8b867403
Jesus Antonio Diaz <jesusantonio30122016@gmail.com>
2019-05-31 18:37:15 -0500

    +Commands

--- Lab_Assistiant_Omega.py
+++ Lab_Assistiant_Omega.py
@@ -23,17 +23,46 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith(prefix+'hello'):
-        if random.randint(0,1)==0:
-            await message.channel.send('Hello!')
-        else:
-            await message.channel.send('Hi!')
+        
     if message.content.startswith(prefix+'ping'):
         await message.channel.send('Pong!')
+    if message.content.startswith(prefix+'help'):
+        await message.channel.send('''
+List of Commands:
+dosupdates - N/A - Get latest updates about DOS ChemMixer.
+ping - N/A - Sample command.
+updates - N/A - Get latest updates about ChemMixer.
+verinfo - N/A - Get version information
+wikipedia - [article name] - Searches in wikipedia the specified article name.
+        ''')
+    if message.content.startswith(prefix+'verinfo'):
+        await message.channel.send('Current Version: 0.4.0b')
+    if message.content.startswith(prefix+'updates'):
+        await message.channel.send('''
+*-* Updates Bulletin N.1 31/05/2019 *-*
+Chemixer v0.4.0b
+-------------------------
++Lithium, Flourine and Sodium derivates
++Bugfixes
+-Career mode
+        ''')
+    if message.content.startswith(prefix+'dosupdates'):
+        await message.channel.send('''
+*-* Updates Bulletin N.1 31/05/2019 *-*
+Chemixer v0.4.0 (DOS)
+-------------------------
++Minus memory use
++Arrow keys navigation
++Better UI
++Compatible with all Text-mode comaptible graphic cards/drivers
++Tested on MS-DOS 6.22
+-Narrow menu
++Scrollbar (WIP)
+        ''')
     if message.content.startswith(prefix+'wikipedia'):
         msgcontent=message.content
         msgcontent=msgcontent.split(' ')[1:]
         msgcontent=cap_first_letter('_'.join(msgcontent))
         await message.channel.send('https://en.wikipedia.org/wiki/'+msgcontent)
+        
 client.run(token)
